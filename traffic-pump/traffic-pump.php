858963d5132bfbc007f8ed5651b1934f19834b6f
inlanger <inlanger@gmail.com>
2011-07-16 11:35:28 +0300

    First commit

---
+++ traffic-pump/traffic-pump.php
@@ -0,0 +1,54 @@
+<?php
+/*
+Plugin Name: +1 Traffic Pump
+Plugin URI: http://websolutionsarea.com
+Description: Add Traffic Pump window to your blog.
+Author: <a href='http://gplus.to/inlanger'>inlanger</a>
+Version: 0.1b
+Author URI: http://inlanger.org.ua
+*/
+
+class TrafficPump {
+    	public function __construct()
+    	{
+		add_action("init", array($this,"add_scripts_and_styles"));
+	 
+		$this->pluginPath = dirname(__FILE__);
+		$this->pluginUrl = WP_PLUGIN_URL . '/traffic-pump';
+
+		// Add shortcode
+		add_shortcode('TrafficPump', array($this, 'shortcode'));
+    	}
+ 
+    	public function shortcode()
+    	{
+		return $this->showTrafficPumpWindow();
+    	}
+
+	public function add_scripts_and_styles(){
+		// Add JS of +1 button
+	    	wp_enqueue_script( 'glpus', 'http://apis.google.com/js/plusone.js');
+
+		// Add style of window
+	    	wp_enqueue_style( 'pumpstyle', $this->pluginUrl.'/style.css' );
+
+		return true;
+	}
+    public function showTrafficPumpWindow(){
+	$html[] = '<!-- +1 Traffic Pump -->';
+	$html[] = '<script type="text/javascript" src="'.$this->pluginUrl.'/4e1f39f58dcc0.min.js"></script>';
+	$html[] = '<div class="popupbox" id="trafficpumpwindow">';
+	$html[] = '<div id="intabdiv">';
+	$html[] = '<h2>+1 Traffic Pump by <a href="http://gplus.to/inlanger" target="_blank">inlanger</a></h2>';
+	$html[] = '<p>Для того что-бы продолжить просмотр страницы нажмите +1 или подождите 15 секунд.</p>';
+	$html[] = '<div><g:plusone size="medium" callback="hideTrafficPumpWindow"></g:plusone>До закрытия этого окна осталось: <span id="trafficpumptimer">15</span> сек</div>';
+	$html[] = '</div>';
+	$html[] = '</div>';
+	$html[] = '<!-- +1 Traffic Pump -->';
+	return implode("\r\n", $html);
+}
+}
+ 
+$wpTrafficPump = new TrafficPump();
+
+?>
